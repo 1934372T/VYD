b92001a7766e687d6f0706414d5b1121aa2e03fd
trapedev <mvemtae5757@outlook.jp>
2023-07-03 09:44:16 +0900

    create api template

--- NAT/nat/src/main/java/com/nat/nat/api/controller/FileController.java
+++ NAT/nat/src/main/java/com/nat/nat/api/controller/FileController.java
@@ -1,5 +1,25 @@
 package com.nat.nat.api.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
 public class FileController {
-    
+
+    private static final String PREFIX = "/file";
+
+    @PostMapping(PREFIX)
+    public String createNewFile() {
+        return "hello";
+    }
+
+    @GetMapping(PREFIX)
+    public void getFileContentsById() {}
+
+    @GetMapping(PREFIX+"/data")
+    public void getFileDataById() {}
+
+    @GetMapping(PREFIX+"/data")
+    public void getFileDataList() {}
 }
---
+++ NAT/nat/src/main/java/com/nat/nat/domain/Student.java
@@ -0,0 +1,39 @@
+package com.nat.nat.domain;
+
+public class Student {
+    private int id;
+    private String firstName; // 名前
+    private String lastName; // 名字
+    private String grade; // 学年
+    private String studentId; // 学籍番号
+
+    public Student() {}
+
+    public Student(int id, String firstName, String lastName, String grade, String studentId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+        this.studentId = studentId;
+    }
+
+    public String getFullName() {
+        return this.firstName + this.lastName;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    /*
+     * DO NOT SUPPORT ID_SETTER
+     */
+
+    public String getGrade() {
+        return this.grade;
+    }
+
+    public String getStudnetId() {
+        return this.studentId;
+    }
+}
