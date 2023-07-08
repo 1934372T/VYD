5f78b536ac8305612d0b963dd0eac770db6e5b72
trapedev <mvemtae5757@outlook.jp>
2023-07-09 04:24:18 +0900

    modified

--- NAT/server/src/main/java/com/nat/nat/Main.java
+++ NAT/server/src/main/java/com/nat/nat/Main.java
@@ -3,6 +3,9 @@ package com.nat.nat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -10,4 +13,17 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
+}
+
+@Configuration
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://127.0.0.1:3331", "http://localhost:3331", "http://10.32.131.34:3331")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+            .allowCredentials(true);
+    }
 }
