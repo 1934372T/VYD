b9228134554e08027fcd471fe837e2db7bede68f
trapedev <mvemtae5757@outlook.jp>
2023-07-11 14:53:16 +0900

    refact code

--- NAT/server/src/main/java/com/nat/nat/api/repository/interfaces/PresentationRepositoryInterface.java
+++ NAT/server/src/main/java/com/nat/nat/api/repository/interfaces/PresentationRepositoryInterface.java
@@ -2,7 +2,4 @@ package com.nat.nat.api.repository.interfaces;
 
 import com.nat.nat.entity.Presentation;
 
-public interface PresentationRepositoryInterface extends CommonRepositoryInterface<Presentation> {
-    void softDelete(Long id);
-    void hardDelete(Long id);
-}
+public interface PresentationRepositoryInterface extends CommonRepositoryInterface<Presentation> {}
